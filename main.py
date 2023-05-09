de332be28558803bb83458a491b9a0cb00908159
Gabriel Arias <132839839+gabrielariasch@users.noreply.github.com>
2023-05-09 12:11:29 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+#Web Scraping - Grabbing a Class
+
+import bs4
+import lxml #helps us process XML and HTML
+
+from bs4 import BeautifulSoup
+
+import requests
+
+result = requests.get("https://realpython.github.io/fake-jobs/")
+soup = bs4.BeautifulSoup(result.text,"lxml")
+
+print('\n')
+
+print("This are the locations")
+print(soup.select('.location'))
